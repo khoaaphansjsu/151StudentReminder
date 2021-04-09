5cd007a5d480a205e5f0b2d9cd01f305eff267d5
Khoa Phan <70754023+khoaaphansjsu@users.noreply.github.com>
2021-04-08 20:24:57 -0700

    Add files via upload

---
+++ AssignmentDB.java
@@ -0,0 +1,21 @@
+package studentReminder;
+
+import java.util.ArrayList;
+
+public class AssignmentDB {
+	private ArrayList<Assignment> assignments;
+	
+	public AssignmentDB () {}
+	
+	//Must figure out how to obtain assignment from assignment DB
+//	public Assignment getAssignment {
+//		return
+//	} 
+	
+	//Must figure out how to delete (and return  
+	//deleted Assignment) assignment from assignment DB
+//	public Assignment deleteAssignment {
+//		return
+//	}
+	
+}
